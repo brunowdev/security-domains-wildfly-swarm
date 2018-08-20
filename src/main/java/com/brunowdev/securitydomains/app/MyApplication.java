e44ae68c80fb09308cd4865258e8d4f339f174b2
Bruno Bitencourt Luiz <brunowdev@gmail.com>
2018-08-19 22:02:18 -0300

    Creating project

---
+++ src/main/java/com/brunowdev/securitydomains/app/MyApplication.java
@@ -0,0 +1,8 @@
+package com.brunowdev.securitydomains.app;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class MyApplication extends Application {
+}
